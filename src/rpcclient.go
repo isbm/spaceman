6ea17b48d3168d8eeb70f1f174489afae46c1620
Bo Maryniuk <bo@suse.de>
2019-07-22 21:30:36 +0200

    Implement authenticated session store, bypass SSL verification

--- src/rpcclient.go
+++ src/rpcclient.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"log"
-
-	"github.com/mattn/go-xmlrpc"
+	"crypto/tls"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
 )
 
 // RPC client object to call the XML-RPC server
@@ -11,25 +14,59 @@ type rpcClient struct {
 	url      string
 	user     string
 	password string
+	session  string
 }
 
 // RPCClient object constructor
-func RPCClient(url string, user string, password string) *rpcClient {
+func RPCClient(url string, user string, password string, insecure bool) *rpcClient {
+	HttpClient = &http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: insecure}}, Timeout: 10 * time.Second}
 	client := new(rpcClient)
 	client.url = url
 	client.user = user
 	client.password = password
+	client.session, _ = client.getSession()
 	return client
+}
+
+// Store session into the file
+func (client *rpcClient) storeSession() error {
+	return ioutil.WriteFile(configuration.session, []byte(client.session+"\n"), 0600)
+}
+
+// Get stored session
+func (client *rpcClient) getSession() (string, error) {
+	var err error
+	var session string
+	if !fileExists(configuration.session) {
+		session = ""
+		err = errors.New("Session file does not exists")
+	} else {
+		data, err := ioutil.ReadFile(configuration.session)
+		Console.checkError(err)
+		session = strings.TrimSpace(string(data))
+	}
+
+	return session, err
+}
+
+func (client *rpcClient) auth() {
+	client.session = client.requestFuction("auth.login", client.user, client.password).(string)
+	Console.checkError(client.storeSession())
 }
 
 /*
 Request a function call on the remote.
 */
-func (client rpcClient) requestFuction(name string, args ...interface{}) (v interface{}) {
-	ret, err := xmlrpc.Call(client.url, name, args...)
-	if err != nil {
-		log.Fatal(err)
+func (client *rpcClient) requestFuction(name string, args ...interface{}) (v interface{}) {
+	var ret interface{}
+	var err error
+
+	ret, err = Call(client.url, name, args...)
+	if err != nil && err.Error() == "invalid response: missing params" {
+		client.auth()
+		ret, err = Call(client.url, name, args...) // Call it again, refreshed session
 	}
+	Console.checkError(err)
 	return ret
 }
 
