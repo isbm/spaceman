f05d48180d2f8c6ebe1e496de992cbeaae75e4e3
Bo Maryniuk <bo@suse.de>
2019-07-23 18:42:24 +0200

    Add labeler object

--- src/util.go
+++ src/util.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
+
+	"github.com/aybabtme/rgbterm"
 )
 
 // Check if file exists
@@ -11,6 +15,44 @@ func fileExists(path string) bool {
 	return info != nil && !info.IsDir()
 }
 
+// Labels object
+type labels struct {
+	addColon bool
+	r        uint8
+	g        uint8
+	b        uint8
+	colored  bool
+}
+
+func NewLabels(addColon bool, r, g, b uint8) *labels {
+	lbl := new(labels)
+	lbl.addColon = addColon
+	lbl.r, lbl.g, lbl.b = r, g, b
+	lbl.colored = true
+
+	return lbl
+}
+
+func (lbl *labels) mapKeyToLabel(label string) string {
+	unCamel, _ := regexp.Compile(`([A-Z]+)`)
+	descore, _ := regexp.Compile(`[_]`)
+	label = strings.Title(strings.ToLower(strings.TrimSpace(unCamel.ReplaceAllString(label, " $1"))))
+	if lbl.colored {
+		label = rgbterm.FgString(label, lbl.r, lbl.g, lbl.b)
+	}
+	label = descore.ReplaceAllString(label, " ")
+	if lbl.addColon {
+		label += ":"
+	}
+	return label
+}
+
+// Set coloring
+func (lbl *labels) setColored(isColored bool) {
+	lbl.colored = isColored
+}
+
+// Console object
 type console struct {
 	hint string
 }
